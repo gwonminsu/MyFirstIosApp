e951414a896d8f5646830b94a2d838a3b767e253
권민수 <kd0615@naver.com>
2023-03-23 11:45:24 +0900

    내 첫번째 앱의 확장버전

--- MyFirstIosApp/ViewController.swift
+++ MyFirstIosApp/ViewController.swift
@@ -12,13 +12,17 @@ class ViewController: UIViewController {
     // !는 나중에 값이 추가될 수 있다는 것을 명시하는 것, 값은 없는데 옵셔널 변수를 강제해제하겠다.
     @IBOutlet var txtName: UITextField!
     
+    @IBOutlet var txtMajor: UITextField!
+    
+    @IBOutlet var txtStudentNum: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func btnSend(_ sender: UIButton) {
-        lblHello.text = "Hello, " + txtName.text!
+        lblHello.text = "Hello, " + txtName.text! + "\n" + txtMajor.text! + "\n" + txtStudentNum.text!
     }
     
 }
